e352f59eb5f0ba7380ce64227e5c96358e2cd262
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-10-15 18:03:07 -0300

    feat: creating the basic layout

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,25 @@
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import { clsx } from "clsx";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({
+  children,
+  asChild,
+  className,
+  ...props
+}: ButtonProps) {
   return (
-    <button className="py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white">
+    <button
+      className={clsx(
+        "py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white",
+        className
+      )}
+      {...props}
+    >
       {children}
     </button>
   );
--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,11 +1,14 @@
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 
-export interface CheckBoxProps {}
+export interface CheckBoxProps extends CheckboxPrimitive.CheckboxProps {}
 
-export function Checkbox({}: CheckBoxProps) {
+export function Checkbox(props: CheckBoxProps) {
   return (
-    <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+    <CheckboxPrimitive.Root
+      className="w-6 h-6 p-[2px] bg-gray-800 rounded"
+      {...props}
+    >
       <CheckboxPrimitive.Indicator asChild>
         <Check weight="bold" className="h-5 w-5 text-cyan-500" />
       </CheckboxPrimitive.Indicator>
